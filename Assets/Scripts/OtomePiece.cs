978ec709c92dc34e88323783b3eaf0ac6a6568bb
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2016-11-09 22:16:20 +0300

    Story piece modal

---
+++ Assets/Scripts/OtomePiece.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OtomePiece {
+
+
+    public string Text;
+    public string LeftSprite;
+    public string RightSprite;
+    public string BackgroundSprite;
+    public string NextPiece;
+
+    public string StartCode;
+    public string EndCode;
+
+
+
+
+
+}
